119a98d8825ab05b4b985ddfdd992ce446c4b815
richard <richard.li@akqa.com>
2015-03-12 14:12:21 +0800

    render url params in json

--- src/response/json.js
+++ src/response/json.js
@@ -1,6 +1,8 @@
 (function () {
     'use strict';
 
+    var _ = require('underscore');
+
     var header = function (env, res) {
         res.writeHead(200,
             {
@@ -12,8 +14,17 @@
     };
 
     var run = function (res, env, rule) {
+        var content = JSON.stringify(rule.response.json);
+
+        content = content.replace(/\{\{([0-9|a-z|A_Z]+)\}\}/g, function (s, v) {
+            var i = _.findLastIndex(rule.request.urlParams, function (p) {
+                return _.isEqual(p[0], v);
+            });
+            return i > -1 ? rule.request.urlParams[i][1] : "";
+        });
+
         header(env, res);
-        res.end(JSON.stringify(rule.response.json));
+        res.end(content);
     };
 
     module.exports.run = run;
